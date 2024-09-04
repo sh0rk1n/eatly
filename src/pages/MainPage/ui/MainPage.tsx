d32cbb06d9fdcab33753ca88c7cdd9366a73effa
sh0rk1n <sh0rk1n@mail.ru>
2024-09-05 00:04:32 +0300

    add json-server (fake api) and simple layout for main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,15 @@
 import React, { memo } from "react";
+import styles from "./MainPage.module.scss";
 
 export const MainPage = memo(() => {
-  return <div>Здесь находится главная страница</div>;
+  return (
+    <div className={styles.root}>
+      <h1>ВХОД В АККАУНТ</h1>
+      <div className={styles.inputs}>
+        <input placeholder="ВВЕДИТЕ ЛОГИН: " />
+        <input placeholder="ВВЕДИТЕ ПАРОЛЬ: " />
+      </div>
+      <button className={styles.button}>ВОЙТИ</button>
+    </div>
+  );
 });
