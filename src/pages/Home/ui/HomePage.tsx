f8c2597b33eba0bbf0428f6b8dde5a2841401744
sh0rk1n <sh0rk1n@mail.ru>
2024-09-15 23:06:10 +0300

    refactoring scss & html

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { Slider } from "widgets/Slider";
 import { MyButton } from "shared/ui/Button/Button";
 import logo from "shared/assets/images/logo.svg";
 import vk from "shared/assets/images/vk-logo.png";
@@ -16,9 +19,6 @@ import food_2 from "shared/assets/images/food-2.svg";
 import food_3 from "shared/assets/images/food-3.svg";
 import bookmark from "shared/assets/images/bookmark.svg";
 import cls from "pages/Home/ui/HomePage.module.scss";
-import { Link } from "react-router-dom";
-import { Slider } from "widgets/Slider";
-import { Box, Container, Stack, Typography, useTheme } from "@mui/material";
 
 export const HomePage = () => {
   /*const navigate = useNavigate();
@@ -30,6 +30,21 @@ export const HomePage = () => {
     logout();
     navigate("/login");
   };*/
+
+  const featureItems = [
+    {
+      number: "10K+",
+      text: "Satisfied Costumers \nAll Great Over The World",
+    },
+    {
+      number: "4M",
+      text: "Healthy Dishes Sold \nIncluding Milk Shakes Smooth",
+    },
+    {
+      number: "99.99%",
+      text: "Reliable Customer \nSupport We Provide Great Experiences",
+    },
+  ];
 
   return (
     <main className="">
@@ -86,43 +101,33 @@ export const HomePage = () => {
         </Container>
       </section>
       <div className={cls.features}>
-        <div className="container">
+        <Container maxWidth="lg">
           <ul className={cls.list}>
-            <li>
-              <div>10K+</div>
-              <p>
-                Satisfied Costumers <br />
-                All Great Over The World
-              </p>
-            </li>
-            <li>
-              <div>4M</div>
-              <p>
-                Healthy Dishes Sold <br />
-                Including Milk Shakes Smooth
-              </p>
-            </li>
-            <li>
-              <div>99.99%</div>
-              <p>
-                Reliable Customer <br />
-                Support We Provide Great Experiences
-              </p>
-            </li>
+            {featureItems.map((item, index) => (
+              <li key={index}>
+                <Typography variant="h3" className={cls.number}>
+                  {item.number}
+                </Typography>
+                <Typography variant="body2" className={cls.text}>
+                  {item.text}
+                </Typography>
+              </li>
+            ))}
           </ul>
-        </div>
+        </Container>
       </div>
       <section className={cls.download}>
-        <div
-          className={`container container-padding ${cls.download_container}`}
+        <Container
+          maxWidth="lg"
+          className={`container-padding ${cls.download_container}`}
         >
-          <div className={cls.image}>
+          <Box className={cls.image}>
             <img src={mobile} alt="" />
-          </div>
+          </Box>
           <div className={cls.content}>
-            <h2>
+            <Typography variant="h2">
               Premium <span>Quality</span> For Your Health
-            </h2>
+            </Typography>
             <ul>
               <li>
                 Premium quality food is made with ingredients that are packed
@@ -137,64 +142,91 @@ export const HomePage = () => {
               Download <img src={button_arrow} alt="" />
             </MyButton>
           </div>
-        </div>
+        </Container>
       </section>
       <section className={cls.restaurants}>
-        <div className={`container container-padding`}>
-          <h2>
+        <Container maxWidth="lg" className={`container-padding`}>
+          <Typography variant="h2">
             Our Top <span>Restaurants</span>
-          </h2>
+          </Typography>
           <ul>
             <li className={cls.item}>
-              <div className={cls.res_img}>
+              <Box className={cls.res_img}>
                 <img src={res_1} alt="" />
-              </div>
-              <div className={cls.info}>
-                <div className={`${cls.tag} ${cls.yellow}`}>Healthy</div>
-                <h3>The Chicken King</h3>
-                <div className={cls.bottom}>
-                  <div className={cls.time}>24min •</div>
-                  <div className={cls.raiting}>4.8</div>
+              </Box>
+              <Box className={cls.info}>
+                <Typography
+                  variant="subtitle1"
+                  className={`${cls.tag} ${cls.yellow}`}
+                >
+                  Healthy
+                </Typography>
+                <Typography variant="h3">The Chicken King</Typography>
+                <Box className={cls.bottom}>
+                  <Typography variant="body1" className={cls.time}>
+                    24min •
+                  </Typography>
+                  <Typography variant="body2" className={cls.raiting}>
+                    4.8
+                  </Typography>
                   <button>
                     <img src={bookmark} alt="" />
                   </button>
-                </div>
-              </div>
+                </Box>
+              </Box>
             </li>
             <li className={cls.item}>
-              <div className={cls.res_img}>
+              <Box className={cls.res_img}>
                 <img src={res_2} alt="" />
-              </div>
-              <div className={cls.info}>
-                <div className={`${cls.tag} ${cls.red}`}>Trending</div>
-                <h3>The Burger King</h3>
-                <div className={cls.bottom}>
-                  <div className={cls.time}>24min •</div>
-                  <div className={cls.raiting}>4.9</div>
+              </Box>
+              <Box className={cls.info}>
+                <Typography
+                  variant="subtitle1"
+                  className={`${cls.tag} ${cls.red}`}
+                >
+                  Trending
+                </Typography>
+                <Typography variant="h3">The Burger King</Typography>
+                <Box className={cls.bottom}>
+                  <Typography variant="body1" className={cls.time}>
+                    24min •
+                  </Typography>
+                  <Typography variant="body2" className={cls.raiting}>
+                    4.9
+                  </Typography>
                   <button>
                     <img src={bookmark} alt="" />
                   </button>
-                </div>
-              </div>
+                </Box>
+              </Box>
             </li>
             <li className={cls.item}>
-              <div className={cls.res_img}>
+              <Box className={cls.res_img}>
                 <img src={res_1} alt="" />
-              </div>
-              <div className={cls.info}>
-                <div className={`${cls.tag} ${cls.yellow}`}>Healthy</div>
-                <h3>The Chicken King</h3>
-                <div className={cls.bottom}>
-                  <div className={cls.time}>24min •</div>
-                  <div className={cls.raiting}>4.8</div>
+              </Box>
+              <Box className={cls.info}>
+                <Typography
+                  variant="subtitle1"
+                  className={`${cls.tag} ${cls.yellow}`}
+                >
+                  Healthy
+                </Typography>
+                <Typography variant="h3">The Chicken King</Typography>
+                <Box className={cls.bottom}>
+                  <Typography variant="body1" className={cls.time}>
+                    24min •
+                  </Typography>
+                  <Typography variant="body2" className={cls.raiting}>
+                    4.8
+                  </Typography>
                   <button>
                     <img src={bookmark} alt="" />
                   </button>
-                </div>
-              </div>
+                </Box>
+              </Box>
             </li>
           </ul>
-          <div className={cls.view}>
+          <Box className={cls.view}>
             <Link to={"/"}>
               View all{" "}
               <svg viewBox="0 0 19 19" xmlns="http://www.w3.org/2000/svg">
@@ -212,35 +244,44 @@ export const HomePage = () => {
                 ></path>
               </svg>
             </Link>
-          </div>
-        </div>
+          </Box>
+        </Container>
       </section>
       <section className={cls.dishes}>
-        <div className="container container-padding">
-          <h2>
+        <Container maxWidth="lg" className="container-padding">
+          <Typography variant="h2">
             Our Top <span>Dishes</span>
-          </h2>
+          </Typography>
           <ul>
             <li>
-              <div className={cls.img}>
+              <Box className={cls.img}>
                 <img src={food_1} />
-              </div>
-              <div className={`${cls.tag} ${cls.yellow}`}>Healthy</div>
-              <h3>Chicken Hell</h3>
-              <div className={cls.info}>
-                <div className={cls.time}>24min •</div>
-                <div className={cls.raiting}>4.9</div>
-              </div>
-              <div className={cls.bottom}>
-                <div className={cls.price}>
+              </Box>
+              <Typography
+                variant="subtitle1"
+                className={`${cls.tag} ${cls.yellow}`}
+              >
+                Healthy
+              </Typography>
+              <Typography variant="h3">Chicken Hell</Typography>
+              <Box className={cls.info}>
+                <Typography variant="body1" className={cls.time}>
+                  24min •
+                </Typography>
+                <Typography variant="body2" className={cls.raiting}>
+                  4.9
+                </Typography>
+              </Box>
+              <Box className={cls.bottom}>
+                <Typography variant="body1" className={cls.price}>
                   $12.<span>99</span>
-                </div>
+                </Typography>
                 <button>
                   <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                     <path d="M15.2818 9.36025H8.94222V15.7891H6.41234V9.36025H0.102539V7.06848H6.41234V0.609863H8.94222V7.06848H15.2818V9.36025Z"></path>
                   </svg>
                 </button>
-              </div>
+              </Box>
             </li>
             <li>
               <div className={cls.img}>
@@ -346,88 +387,97 @@ export const HomePage = () => {
               </svg>
             </Link>
           </div>
-        </div>
+        </Container>
       </section>
       <section className={cls.purchase}>
-        <div
-          className={`container container-padding ${cls.purchase_container}`}
+        <Container
+          maxWidth="lg"
+          className={`container-padding ${cls.purchase_container}`}
         >
-          <div className={cls.header}>
-            <h2>
+          <Box className={cls.header}>
+            <Typography variant="h2">
               Control <span>Purchases</span> <br /> Via Dashboard
-            </h2>
+            </Typography>
             <ul>
               <li>
-                <div className={cls.img}>
+                <Box className={cls.img}>
                   <img src={food_1} />
-                </div>
-                <div className={cls.info}>
-                  <h3>Chicken Hell</h3>
-                  <p>On The Way</p>
-                  <div>3:09 PM</div>
-                </div>
+                </Box>
+                <Box className={cls.info}>
+                  <Typography variant="h3">Chicken Hell</Typography>
+                  <Typography variant="body1">On The Way</Typography>
+                  <Typography variant="body2">3:09 PM</Typography>
+                </Box>
               </li>
               <li>
-                <div className={cls.img}>
+                <Box className={cls.img}>
                   <img src={food_2} />
-                </div>
-                <div className={cls.info}>
-                  <h3>Swe Dish</h3>
-                  <p className={cls.delivered}>Delivered</p>
-                  <div>Yesterday</div>
-                </div>
+                </Box>
+                <Box className={cls.info}>
+                  <Typography variant="h3">Swe Dish</Typography>
+                  <Typography variant="body1" className={cls.delivered}>
+                    Delivered
+                  </Typography>
+                  <Typography variant="body2">Yesterday</Typography>
+                </Box>
               </li>
               <li>
-                <div className={cls.img}>
+                <Box className={cls.img}>
                   <img src={food_3} />
-                </div>
-                <div className={cls.info}>
-                  <h3>Fish Hell Veg</h3>
-                  <p className={cls.cancelled}>Cancelled</p>
-                  <div>Yesterday</div>
-                </div>
+                </Box>
+                <Box className={cls.info}>
+                  <Typography variant="h3">Fish Hell Veg</Typography>
+                  <Typography variant="body1" className={cls.cancelled}>
+                    Cancelled
+                  </Typography>
+                  <Typography variant="body2">Yesterday</Typography>
+                </Box>
               </li>
             </ul>
-          </div>
-          <div className={cls.purchaseGraph}>
-            <div className={cls.header}>
-              <h3>Purchases</h3>
+          </Box>
+          <Box className={cls.purchaseGraph}>
+            <Box className={cls.header}>
+              <Typography variant="h3">Purchases</Typography>
               <select name="timeline">
                 <option value="mounth">This month</option>
                 <option value="week">This week</option>
                 <option value="year">This year</option>
               </select>
-            </div>
+            </Box>
             <ul className={cls.info}>
               <li className={cls.graphPurple}>
-                <div className={cls.wrapper}>
+                <Box className={cls.wrapper}>
                   <span className={cls.graphIcon}></span>
-                  <div className={cls.about}>
-                    <h4>Expense</h4>
-                    <p>Increased By 10%</p>
-                  </div>
-                </div>
-                <div className={cls.price}>$409.00</div>
+                  <Box className={cls.about}>
+                    <Typography variant="h4">Expense</Typography>
+                    <Typography variant="body1">Increased By 10%</Typography>
+                  </Box>
+                </Box>
+                <Typography variant="body1" className={cls.price}>
+                  $409.00
+                </Typography>
               </li>
               <li className={cls.graphYellow}>
-                <div className={cls.wrapper}>
+                <Box className={cls.wrapper}>
                   <span className={cls.graphIcon}></span>
-                  <div className={cls.about}>
-                    <h4>Vocher Usage</h4>
-                    <p>Increased By 5%</p>
-                  </div>
-                </div>
-                <div className={cls.price}>$45.78</div>
+                  <Box className={cls.about}>
+                    <Typography variant="h4">Vocher Usage</Typography>
+                    <Typography variant="body1">Increased By 5%</Typography>
+                  </Box>
+                </Box>
+                <Typography variant="body1" className={cls.price}>
+                  $45.78
+                </Typography>
               </li>
             </ul>
-          </div>
-        </div>
+          </Box>
+        </Container>
       </section>
       <Slider />
       <div className={cls.subscribe}>
-        <div className="container">
-          <div className={cls.wrapper}>
-            <h2>Get 50%</h2>
+        <Container maxWidth="lg">
+          <Box className={cls.wrapper}>
+            <Typography variant="h2">Get 50%</Typography>
             <form action="#">
               <input type="email" placeholder="Enter Your Email Address" />
               <div>
@@ -440,12 +490,12 @@ export const HomePage = () => {
                 </MyButton>
               </div>
             </form>
-          </div>
-        </div>
+          </Box>
+        </Container>
       </div>
       <footer className={cls.footer}>
-        <div className="container">
-          <div className={cls.header}>
+        <Container maxWidth="lg">
+          <Box className={cls.header}>
             <Link to={"/"}>
               <img src={logo} alt="" />
               <span>eatly</span>
@@ -464,9 +514,11 @@ export const HomePage = () => {
                 <Link to={"/"}>Contacts</Link>
               </li>
             </ul>
-          </div>
-          <div className={cls.bottom}>
-            <p>© 2024 EATLY All Rights Reserved.</p>
+          </Box>
+          <Box className={cls.bottom}>
+            <Typography variant="body1">
+              © 2024 EATLY All Rights Reserved.
+            </Typography>
             <ul>
               <li>
                 <Link to={"/"}>
@@ -489,8 +541,8 @@ export const HomePage = () => {
                 </Link>
               </li>
             </ul>
-          </div>
-        </div>
+          </Box>
+        </Container>
       </footer>
     </main>
   );
